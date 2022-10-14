7d29ff7f47dd89a7573004c154376b2d8d68eb5a
Miqueas-Daniel <115736585+Miqueas-Daniel@users.noreply.github.com>
2022-10-14 08:45:01 -0300

    Update info-proyectos.js

--- src/info-proyectos.js
+++ src/info-proyectos.js
@@ -2,28 +2,28 @@ let projectsInfo = [
     {
         id: 0,
         title: 'Puzzle Básico',
-        img: '../images/proyecto_1.jpg',
+        img: './images/proyecto_1.jpg',
         imgTitle: '',
         description: 'Es realmente básico. Estaba aprendiendo Drag and Drop (arrastrar y soltar) y salió este, "mini puzzle para bebés" se le podría decir, lo hize adaptable a dispositivos móbiles pero tristemente no funciona en celulares :('
     },
     {
         id: 1,
         title: 'Título de ejemplo',
-        img: '../images/proximamente.jpg',
+        img: './images/proximamente.jpg',
         imgTitle: 'próximamente',
         description: 'Esta es una descripción de ejemplo, cuando tenga nuevos mini proyectos voy a estar subiéndolos y dejando el enlace en la lista de PROYECTOS.'
     },
     {
         id: 2,
         title: 'Título de ejemplo',
-        img: '../images/proximamente.jpg',
+        img: './images/proximamente.jpg',
         imgTitle: 'próximamente',
         description: 'Esta es una descripción de ejemplo, cuando tenga nuevos mini proyectos voy a estar subiéndolos y dejando el enlace en la lista de PROYECTOS.'
     },
     {
         id: 3,
         title: 'Título de ejemplo',
-        img: '../images/proximamente.jpg',
+        img: './images/proximamente.jpg',
         imgTitle: 'próximamente',
         description: 'Esta es una descripción de ejemplo, cuando tenga nuevos mini proyectos voy a estar subiéndolos y dejando el enlace en la lista de PROYECTOS.'
     },
